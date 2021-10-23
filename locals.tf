576bd1df02a0d27c807865731a60fa79858901ed
Robert de Bock <robert@meinit.nl>
2021-10-23 20:35:52 +0200

    Remove unused mapping and explain more about mapping.

--- locals.tf
+++ locals.tf
@@ -1,13 +1,4 @@
 locals {
-  # TODO: idea, decide instance amount based on cluster type?
-  #
-  # _instance_amount = {
-  #   development = 1
-  #   small = 3
-  #   large = 5
-  #   maximum = 7
-  # }
-  # instance_amount = local._instance_amount[var.size]
 
   _instance_type = {
     development = "t3.micro"
@@ -27,6 +18,7 @@ locals {
   }
   volume_size = local._volume_size[var.size]
 
+  # TODO: Solve this in a data object with filtering.
   _instance_ami = {
     eu-central-1 = "ami-0233214e13e500f77"
     eu-west-1    = "ami-047bb4163c506cd98"
@@ -44,28 +36,29 @@ locals {
   }
   associate_public_ip_address = local._associate_public_ip_address[var.size]
 
-  # Mpas protocols from aws_lb_target_group to aws_lb_target_group
-  _health_check = {
-    GENEVE  = "TCP"
-    HTTP    = "HTTP"
-    HTTPS   = "HTTPS"
-    TCP_UDP = "TCP" # Healthchecking is not possible for UDP.
-    TCP     = "TCP"
-    TLS     = "TCP"
-    UDP     = "TCP" # Healthchecking is not possible for UDP.
-  }
+  # aws_lb_target_group.protocol can be:
+  # GENEVE, HTTP, HTTPS, TCP, TCP_UDP, TLS, or UDP.
+
+  # aws_lb_listener.protocol can be:
+  # TCP, TLS, UDP, and TCP_UDP.
 
   # Maps protocols from aws_lb_target_group to aws_lb_listerer
   _listener_protocol = {
     GENEVE  = "TCP"
     HTTP    = "TCP"
-    HTTPS   = "TCP"
+    HTTPS   = "TLS"
     TCP     = "TCP"
     TCP_UDP = "TCP_UDP"
     TLS     = "TLS"
     UDP     = "UDP"
   }
 
+  # aws_lb_target_group.protocol can be:
+  # GENEVE, HTTP, HTTPS, TCP, TCP_UDP, TLS, or UDP.
+
+  # aws_securityu_group_rule.protocol can be:
+  # icmp, icmpv6, tcp, udp, all or use the protocol number.
+  
   # Maps protocols from aws_lb_target_group to security_group_rule.
   _security_group_rule_protocol = {
     GENEVE  = "TCP"
